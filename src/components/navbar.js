26400fc1e2e25aa3ee58baafefef6a110b3254bf
avilla2 <a.villa12@outlook.com>
2021-10-06 11:39:47 -0700

    comments

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -17,7 +17,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import { Link } from "react-router-dom";
 
-
+// I am modifying
 const useStyles = makeStyles((theme) => ({
     offset: theme.mixins.toolbar,
     root: {
